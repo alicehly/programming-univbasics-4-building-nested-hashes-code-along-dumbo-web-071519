ff250c498ea18c1f033603761d1db3925479b7c0
Alice Ly Ms <alicehly@gmail.com>
2019-07-09 20:59:38 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,7 @@ def second_challenge
       :patriarch => { :name => "Lord Montague", :age => "54"}
       :matriarch => {:name => "Lady Motague", :age => "54"}
       :hero => {:name => "Romeo", :age => "15", :status => "alive"}
-      :hero_friends => []
+      :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"}, {:name => "Mercutio". :age => "18", :attitude => "hot-headed"}]
       
       }, 
     :capulet => {
@@ -13,7 +13,7 @@ def second_challenge
       :patriarch => {:name => "Lord Capulet", :age => "50"}
       :matriarch => {:name => "Lady Capulet", :age => "51"}
       :heroine => {:name => "Juliet", :age =>"15", :status => "alive"}
-      :heroine_friends => []
+      :heroine_friends => [{:name => "Steven", :age => "30", :attitude => "confused"}, {:name => "Nurse", :age =>"44", :attitude => "worried"}]
       }
     
   }
